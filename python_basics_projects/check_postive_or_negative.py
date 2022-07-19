69f8731579ad3311534ee2d2800374f8d6ab0b70
Kizito3A <96149225+Kizito3A@users.noreply.github.com>
2022-07-19 04:31:09 -0700

    Update check_postive_or_negative.py

--- python_basics_projects/check_postive_or_negative.py
+++ python_basics_projects/check_postive_or_negative.py
@@ -1,1 +1,9 @@
-print('ada')
+#This program checks wether an integer is positive, negative or zero
+
+num = int(input('Enter your number: '))
+if num > 0:
+    print('It is a postive number')
+elif num < 0:
+    print('This is negative')
+else:
+    print('The number is zero')
