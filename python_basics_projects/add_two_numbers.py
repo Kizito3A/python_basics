75f4ab7adf976863181363bf9edad2363269953f
Kizito3A <96149225+Kizito3A@users.noreply.github.com>
2022-07-20 06:24:38 -0700

    I added a file that contain a code that adds two numbers together

---
+++ python_basics_projects/add_two_numbers.py
@@ -0,0 +1,12 @@
+#python program that adds two numbers 
+
+#Get the input from the user
+from re import T
+
+
+number1 = float(input('Enter the first number: '))
+number2 = float(input('Enter the second number: '))
+
+sum = number1 + number2
+print("The sum of {0} and {1} is {2}".format(number1, number2, sum))
+
